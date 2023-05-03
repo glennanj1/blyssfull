9e1dc1e741bd109f75d4f2880d7f4af9bea7a87c
glennanj1 <glennan20@gmail.com>
2023-05-02 22:30:34 -0400

    booking logic update to check available dates

--- pages/book.js
+++ pages/book.js
@@ -14,20 +14,26 @@ export default function Book() {
   const [desc, setDesc] = useState();
   const [selectedDate, setSelectedDate] = useState(null);
   const [formValid, setFormValid] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [isSlotAvailable, setIsSlotAvailable] = useState(true);
 
-  const handleDateChange = (date) => {
+  const handleDateChange = async (date) => {
     setSelectedDate(date);
+    setLoading(true);
+    // Call your backend API to check if the selected date and time slot is available
+    const isAvailable = await checkAvailability(date);
+    setIsSlotAvailable(!isAvailable);
+    setLoading(false);
   };
 
   useEffect(() => {
-    console.log(desc)
+    console.log(desc);
     if (selectedDate != null && price != null) {
       setFormValid(false);
     } else {
       setFormValid(true);
     }
-  }, [selectedDate, price, setFormValid, formValid])
-  
+  }, [selectedDate, price, setFormValid, formValid, loading, isSlotAvailable]);
 
   const handleServiceChange = (e) => {
     setPrice(e.target.value);
@@ -41,38 +47,38 @@ export default function Book() {
         {/* <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
         Book
         </h1> */}
-          <div class="md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
-            <div>
-              <div class="md:grid md:grid-cols-3 md:gap-6">
-                <div class="md:col-span-1">
-                  <div class="px-4 sm:px-4">
-                    <h2 className="lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
-                      Book{" "}
-                      <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
-                        Here
-                      </span>
+        <div class="md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
+          <div>
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+              <div class="md:col-span-1">
+                <div class="px-4 sm:px-4">
+                  <h2 className="lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
+                    Book{" "}
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
+                      Here
+                    </span>
+                  </h2>
+                  {price ? (
+                    <h2 className="text-1xl md:text-3xl font-extrabold leading-tighter tracking-tighter mb-4">
+                      Price: ${price}
                     </h2>
-                    {price ? (
-                      <h2 className="text-1xl md:text-3xl font-extrabold leading-tighter tracking-tighter mb-4">
-                        Price: ${price}
-                      </h2>
-                    ) : null}
-                    <p class="mt-1 text-sm text-gray-600">
-                      Enter Some Details & Select a Service
-                    </p>
-                  </div>
+                  ) : null}
+                  <p class="mt-1 text-sm text-gray-600">
+                    Enter Some Details & Select a Service
+                  </p>
                 </div>
-                <div class="mt-5 md:col-span-2 md:mt-0">
-                  <form>
-                    <div className="overflow-hidden shadow sm:rounded-md">
-                      <div className="bg-white px-4 py-5 sm:p-6">
-                        <div className="grid grid-cols-6 gap-6">
-                            {/* <label
+              </div>
+              <div class="mt-5 md:col-span-2 md:mt-0">
+                <form>
+                  <div className="overflow-hidden shadow sm:rounded-md">
+                    <div className="bg-white px-4 py-5 sm:p-6">
+                      <div className="grid grid-cols-6 gap-6">
+                        {/* <label
                               htmlFor="first-name"
                               className="block text-sm font-medium leading-6 text-gray-900"
                             >
                               First name */}
-                            {/* </label>
+                        {/* </label>
                             <input
                               required
                               type="text"
@@ -119,45 +125,60 @@ export default function Book() {
                             />
                           </div> */}
 
-                            <div className="col-span-6 sm:col-start-1 col-end-7">
-                              <label
-                                htmlFor="services"
-                                className="block text-sm font-medium leading-6 text-gray-900"
-                              >
-                                Service
-                              </label>
-                              <select
-                                required
-                                onChange={handleServiceChange}
-                                id="services"
-                                name="services"
-                                value={price}
-                                autoComplete="services-name"
-                                className="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                              >
-                                <option value=""></option>
-                                <option value="15.00" label="Intoductory Session">
-                                  Intoductory Session
-                                </option>
-                                <option value="111.00" label="1 Hour Distance Reiki Session">
-                                  1 Hour Distance Reiki Session
-                                </option>
-                                <option value="55.00" label="30 Minute Distance Reiki Session">
-                                  30 Minute Distance Reiki Session
-                                </option>
-                                <option value="33.00" label="30 Minute Tarot Reading">
-                                  30 Minute Tarot Reading
-                                </option>
-                                <option value="33.00" label="30 Minute Oracle Reading">
-                                  30 Minute Oracle Reading
-                                </option>
-                                <option value="22.00" label="15 Minute Oracle Reading">
-                                  15 Minute Oracle Reading
-                                </option>
-                              </select>
-                            </div>
+                        <div className="col-span-6 sm:col-start-1 col-end-7">
+                          <label
+                            htmlFor="services"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Service
+                          </label>
+                          <select
+                            required
+                            onChange={handleServiceChange}
+                            id="services"
+                            name="services"
+                            value={price}
+                            autoComplete="services-name"
+                            className="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          >
+                            <option value=""></option>
+                            <option value="15.00" label="Intoductory Session">
+                              Intoductory Session
+                            </option>
+                            <option
+                              value="111.00"
+                              label="1 Hour Distance Reiki Session"
+                            >
+                              1 Hour Distance Reiki Session
+                            </option>
+                            <option
+                              value="55.00"
+                              label="30 Minute Distance Reiki Session"
+                            >
+                              30 Minute Distance Reiki Session
+                            </option>
+                            <option
+                              value="33.00"
+                              label="30 Minute Tarot Reading"
+                            >
+                              30 Minute Tarot Reading
+                            </option>
+                            <option
+                              value="33.00"
+                              label="30 Minute Oracle Reading"
+                            >
+                              30 Minute Oracle Reading
+                            </option>
+                            <option
+                              value="22.00"
+                              label="15 Minute Oracle Reading"
+                            >
+                              15 Minute Oracle Reading
+                            </option>
+                          </select>
+                        </div>
 
-                            {/* <div className="col-span-6">
+                        {/* <div className="col-span-6">
                             <label
                               htmlFor="street-address"
                               className="block text-sm font-medium leading-6 text-gray-900"
@@ -174,7 +195,7 @@ export default function Book() {
                             />
                           </div> */}
 
-                            {/* <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        {/* <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                             <label
                               htmlFor="city"
                               className="block text-sm font-medium leading-6 text-gray-900"
@@ -191,7 +212,7 @@ export default function Book() {
                             />
                           </div> */}
 
-                            {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
                             <label
                               htmlFor="region"
                               className="block text-sm font-medium leading-6 text-gray-900"
@@ -208,7 +229,7 @@ export default function Book() {
                             />
                           </div> */}
 
-                            {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
                             <label
                               htmlFor="postal-code"
                               className="block text-sm font-medium leading-6 text-gray-900"
@@ -224,24 +245,36 @@ export default function Book() {
                               className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                           </div> */}
-                            <div className="col-span-6 sm:col-start-1 col-end-7">
-                              <label className="block text-sm font-medium leading-6 text-gray-900">
-                                Pick Date
-                              </label>
-                              <CustomDatePicker onChange={handleDateChange} />
-                            </div>
-                      
-                        </div>
-                        <div className="bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6">
-                            <Paypal isDisabled={formValid} cost={price} date={selectedDate} desc={desc} userId={session.id} />
+                        <div className="col-span-6 sm:col-start-1 col-end-7">
+                          <label className="block text-sm font-medium leading-6 text-gray-900">
+                            Pick Date
+                          </label>
+                          <CustomDatePicker onChange={handleDateChange} />
+                          {loading && <p>Loading availability...</p>}
+                          {!isSlotAvailable && (
+                            <p className="text-red-500">
+                              The selected date and time slot is already booked.
+                              Please choose a different slot.
+                            </p>
+                          )}
                         </div>
                       </div>
+                      <div className="bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6">
+                        <Paypal
+                          isDisabled={formValid}
+                          cost={price}
+                          date={selectedDate}
+                          desc={desc}
+                          userId={session.id}
+                        />
+                      </div>
                     </div>
-                  </form>
-                </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
+        </div>
         <Footer />
       </>
     );
@@ -261,58 +294,66 @@ export default function Book() {
     <>
       <Header isBooking={true} />
       <div class="animate-pulse md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
-            <div>
-              <div class="md:grid md:grid-cols-3 md:gap-6">
-                <div class="md:col-span-1">
-                  <div class="px-4 sm:px-4">
-                    <h2 className="h-40 bg-slate-700 rounded lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
-                     
-                      <span className="h-20 bg-slate-700 rounded bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
-                      
-                      </span>
-                    </h2>
-                    <p class="h-10 bg-slate-700 rounded mt-1 text-sm text-gray-600">
-                    </p>
-                  </div>
-                </div>
-                <div class="mt-5 md:col-span-2 md:mt-0">
-                  <form>
-                    <div className="overflow-hidden shadow sm:rounded-md">
-                      <div className="bg-white px-4 py-5 sm:p-1">
-                        <div className="grid grid-cols-6 gap-6">
-                            <div className="col-span-6 sm:col-start-1 col-end-7">
-                              <label
-                                htmlFor="services"
-                                className="h-5 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"
-                              >
-                                
-                              </label>
-                              <label
-                                htmlFor="services"
-                                className="h-10 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"
-                              >
-                                
-                              </label>
-                            
-                            </div>
-
-                            <div className="col-span-6 sm:col-start-1 col-end-7">
-                              <label className="h-5 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900">
-                              </label>
-                              <label className="h-10 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900">
-                              </label>
-                            </div>
-                      
-                        </div>
-                        <div className="h-20 bg-slate-700 rounded bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6">
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
+        <div>
+          <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+              <div class="px-4 sm:px-4">
+                <h2 className="h-40 bg-slate-700 rounded lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
+                  <span className="h-20 bg-slate-700 rounded bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500"></span>
+                </h2>
+                <p class="h-10 bg-slate-700 rounded mt-1 text-sm text-gray-600"></p>
               </div>
             </div>
+            <div class="mt-5 md:col-span-2 md:mt-0">
+              <form>
+                <div className="overflow-hidden shadow sm:rounded-md">
+                  <div className="bg-white px-4 py-5 sm:p-1">
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6 sm:col-start-1 col-end-7">
+                        <label
+                          htmlFor="services"
+                          className="h-5 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"
+                        ></label>
+                        <label
+                          htmlFor="services"
+                          className="h-10 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"
+                        ></label>
+                      </div>
+
+                      <div className="col-span-6 sm:col-start-1 col-end-7">
+                        <label className="h-5 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"></label>
+                        <label className="h-10 mt-2 bg-slate-700 rounded block text-sm font-medium leading-6 text-gray-900"></label>
+                      </div>
+                    </div>
+                    <div className="h-20 bg-slate-700 rounded bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6"></div>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
+        </div>
+      </div>
     </>
   );
 }
+
+
+async function checkAvailability(reqDate) {
+  const response = await fetch("/api/checkDate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ reqDate }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return data.isWithinTimeSlots;
+  } else {
+    // display new error instead maybe?
+    console.log("Error checking time slot");
+  }
+}
+
+
