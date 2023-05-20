c2682a4931cf4650d6c5fa2cce2059bdb2d1d5a6
glennanj1 <glennan20@gmail.com>
2023-05-20 19:15:27 -0400

    index page updates

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import HomePage from '@/Components/HomePage'
-import Header from '@/Components/Header'
-import HeroHome from '@/Components/Hero'
-import Testimonials from '@/Components/Testimonials'
-import Features from '@/Components/Features'
-import Newsletter from '@/Components/Email'
-import Footer from '@/Components/Footer'
+import Head from "next/head";
+import HomePage from "@/Components/HomePage";
+import Header from "@/Components/Header";
+import HeroHome from "@/Components/Hero";
+import Testimonials from "@/Components/Testimonials";
+import Features from "@/Components/Features";
+import Newsletter from "@/Components/Email";
+import Footer from "@/Components/Footer";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
@@ -14,16 +14,33 @@ export default function Home() {
   const router = useRouter();
 
   if (session?.newUser === true) {
-    router.push('/auth/newUser/')
+    router.push("/auth/newUser/");
   }
 
   return (
     <div className="bg-violet-300">
       <Head>
-        <title>Blyssfull Magick</title>
-        <meta name="description" content="Start your journey today" />
+        <meta charset="utf-8" />
+        <link rel="icon" href="/logo64.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#c4b5fd" />
+        <meta property="og:image" content="/eye6.png" />
+        <meta property="og:title" content="Blyssfull Magick" />
+        <meta
+          property="og:description"
+          content="Through a range of spiritual practices and techniques, our service can help you overcome any emotional or spiritual blockages."
+        />
+        <meta property="og:type" content="website" />
+
+        <meta
+          name="Blyssfull Magick"
+          content="Try an Intro Session Today!"
+        />
+        <title>Blyssfull Magick</title>
+        <link rel="apple-touch-icon" href="/BlyssfullLogo.png" />
+        <link rel="manifest" href="/manifest.json" />
+
+        <link rel="icon" href="/eye6.ico" />
       </Head>
       <Header />
       <HeroHome />
@@ -33,5 +50,5 @@ export default function Home() {
       {/* <Newsletter /> */}
       <Footer />
     </div>
-  )
+  );
 }
