9aa2eee52b6f53a48c10ab4641e42a8b77ae6d74
glennanj1 <glennan20@gmail.com>
2023-05-20 20:28:48 -0400

    test meta tag

--- pages/index.js
+++ pages/index.js
@@ -22,22 +22,27 @@ export default function Home() {
       <Head>
       {/* <!-- Primary Meta Tags --> */}
       <title>Blyssfull Magick</title>
-      <meta name="title" content="Blyssfull Magick"/>
-      <meta name="description" content="Try an Intro Session Today!"/>
+      <meta name="title" content="Blyssfull Magick"></meta>
+      <meta name="description" content="Try an Intro Session Today!"></meta>
 
       {/* <!-- Open Graph / Facebook --/> */}
-      <meta property="og:type" content="website"/>
-      <meta property="og:url" content="https://www.blyssfullmagick.com/"/>
-      <meta property="og:title" content="Blyssfull Magick"/>
-      <meta property="og:description" content="Try an Intro Session Today!"/>
-      <meta property="og:image" content="/metaLogo.png"/>
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content="https://www.blyssfullmagick.com/"></meta>
+      <meta property="og:title" content="Blyssfull Magick"></meta>
+      <meta property="og:description" content="Try an Intro Session Today!"></meta>
+      <meta property="og:image" content="/metaLogo.png"></meta>
 
-      {/* <!-- Twitter --/> */}
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="https://www.blyssfullmagick.com/"/>
-      <meta property="twitter:title" content="Blyssfull Magick"/>
-      <meta property="twitter:description" content="Try an Intro Session Today!"/>
-      <meta property="twitter:image" content="/metaLogo.png"/>
+      {/* <!-- Twitter --></meta> */}
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:url" content="https://www.blyssfullmagick.com/"></meta>
+      <meta property="twitter:title" content="Blyssfull Magick"></meta>
+      <meta property="twitter:description" content="Try an Intro Session Today!"></meta>
+      <meta property="twitter:image" content="/metaLogo.png"></meta>
+
+      {/* Link */}
+      <link rel='icon' type='image/png' href='icon3.ico' />
+      <link rel='icon' sizes='192x192' href='icon3.ico'/>
+      <link rel='apple-touch-icon' href='icon3.ico'/>
       </Head>
       <Header />
       <HeroHome />
