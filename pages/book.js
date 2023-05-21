726cbf409e12ed33fe649553f5af5546f9ddb32e
glennanj1 <glennan20@gmail.com>
2023-05-20 20:59:54 -0400

    logic updates for free promo

--- pages/book.js
+++ pages/book.js
@@ -1,33 +1,90 @@
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import Router from "next/router";
 import { useState, useEffect } from "react";
 import Paypal from "../Components/Paypal";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import CustomDatePicker from "@/Components/CustomDatePicker";
+import Example from "@/Components/ServiceSelection";
 
 export default function Book() {
   const { data: session, status } = useSession();
-  const router = useRouter();
+  //const router = useRouter();
   const [authed, setAuthed] = useState(false);
-  const [price, setPrice] = useState(null);
+  const [price, setPrice] = useState();
   const [desc, setDesc] = useState();
   const [selectedDate, setSelectedDate] = useState(null);
   const [formValid, setFormValid] = useState(true);
   const [loading, setLoading] = useState(false);
   const [isSlotAvailable, setIsSlotAvailable] = useState(true);
+  const [promo, setPromo] = useState(false);
+
+  async function handleSubmit(event, router) {
+    event.preventDefault();
+    let data = {
+      id: Date.now() + Math.floor(Math.random() * 100),
+      intent: "CAPTURE",
+      status: "COMPLETED",
+      purchase_units: [
+        {
+          reference_id: "default",
+          amount: {
+            currency_code: "USD",
+            value: "0",
+          },
+          shipping: {
+            name: {
+              full_name: "Test Name",
+            },
+            address: {
+              address_line_1: "123 Test Drive",
+              admin_area_2: "Test",
+              admin_area_1: "EX",
+              postal_code: "00000",
+              country_code: "US",
+            },
+          },
+        },
+      ],
+      date: selectedDate,
+      desc: desc,
+      userId: session.id,
+    };
+    console.log("Data being sent to API:", data);
+    try {
+      const response = await fetch("/api/booking/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to save transaction");
+        // show a modal that the transaction didn't go through and to try again
+      }
+
+      const savedTransaction = await response.json();
+      // show success and redirect
+      debugger;
+      Router.push(`/booking/${savedTransaction}`);
+      console.log("Transaction saved:", savedTransaction);
+    } catch (error) {
+      console.error("Error saving transaction:", error);
+    }
+  }
 
   const handleDateChange = async (date) => {
     setSelectedDate(date);
     setLoading(true);
     // Call your backend API to check if the selected date and time slot is available
-    const isAvailable = await checkAvailability(date);
+    const isAvailable = await checkAvailability(date, desc);
     setIsSlotAvailable(!isAvailable);
     setLoading(false);
   };
 
   useEffect(() => {
-    console.log(desc);
     if (selectedDate != null && price != null) {
       setFormValid(false);
     } else {
@@ -38,6 +95,15 @@ export default function Book() {
   const handleServiceChange = (e) => {
     setPrice(e.target.value);
     setDesc(e.target[e.target.selectedIndex].text);
+    if (e.target[e.target.selectedIndex].text === "Introductory Session") {
+      setPromo(true);
+    } else if (
+      e.target[e.target.selectedIndex].text !== "Introductory Session"
+    ) {
+      setPromo(false);
+    } else {
+      setPromo(false);
+    }
   };
 
   if (authed) {
@@ -47,11 +113,11 @@ export default function Book() {
         {/* <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
         Book
         </h1> */}
-        <div class="md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
+        <div className="md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
           <div>
-            <div class="md:grid md:grid-cols-3 md:gap-6">
-              <div class="md:col-span-1">
-                <div class="px-4 sm:px-4">
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              <div className="md:col-span-1">
+                <div className="px-4 sm:px-4">
                   <h2 className="lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
                     Book{" "}
                     <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500">
@@ -63,12 +129,12 @@ export default function Book() {
                       Price: ${price}
                     </h2>
                   ) : null}
-                  <p class="mt-1 text-sm text-gray-600">
+                  <p className="mt-1 text-sm text-gray-600">
                     Enter Some Details & Select a Service
                   </p>
                 </div>
               </div>
-              <div class="mt-5 md:col-span-2 md:mt-0">
+              <div className="mt-5 md:col-span-2 md:mt-0">
                 <form>
                   <div className="overflow-hidden shadow sm:rounded-md">
                     <div className="bg-white px-4 py-5 sm:p-6">
@@ -142,8 +208,8 @@ export default function Book() {
                             className="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           >
                             <option value=""></option>
-                            <option value="15.00" label="Intoductory Session">
-                              Intoductory Session
+                            <option value="0.00" label="Introductory Session">
+                              Introductory Session
                             </option>
                             <option
                               value="111.00"
@@ -175,76 +241,88 @@ export default function Book() {
                             >
                               15 Minute Oracle Reading
                             </option>
+                            <option
+                              value="100000.00"
+                              label="15 Minute Oracle Reading"
+                            >
+                              15 Minute Special Reading
+                            </option>
                           </select>
                         </div>
+                        <Example />
 
-                        {/* <div className="col-span-6">
-                            <label
-                              htmlFor="street-address"
-                              className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                              Street address
-                            </label>
-                            <input
-                              required
-                              type="text"
-                              name="street-address"
-                              id="street-address"
-                              autoComplete="street-address"
-                              className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                          </div> */}
+                        {promo ? (
+                          <>
+                            <div className="col-span-6">
+                              <label
+                                htmlFor="street-address"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Street address
+                              </label>
+                              <input
+                                required
+                                type="text"
+                                name="street-address"
+                                id="street-address"
+                                autoComplete="street-address"
+                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
 
-                        {/* <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                            <label
-                              htmlFor="city"
-                              className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                              City
-                            </label>
-                            <input
-                              required
-                              type="text"
-                              name="city"
-                              id="city"
-                              autoComplete="address-level2"
-                              className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                          </div> */}
+                            <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                              <label
+                                htmlFor="city"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                City
+                              </label>
+                              <input
+                                required
+                                type="text"
+                                name="city"
+                                id="city"
+                                autoComplete="address-level2"
+                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
 
-                        {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <label
-                              htmlFor="region"
-                              className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                              State / Province
-                            </label>
-                            <input
-                              required
-                              type="text"
-                              name="region"
-                              id="region"
-                              autoComplete="address-level1"
-                              className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                          </div> */}
+                            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                              <label
+                                htmlFor="region"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                State / Province
+                              </label>
+                              <input
+                                required
+                                type="text"
+                                name="region"
+                                id="region"
+                                autoComplete="address-level1"
+                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
 
-                        {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <label
-                              htmlFor="postal-code"
-                              className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                              ZIP / Postal code
-                            </label>
-                            <input
-                              required
-                              type="text"
-                              name="postal-code"
-                              id="postal-code"
-                              autoComplete="postal-code"
-                              className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                          </div> */}
+                            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                              <label
+                                htmlFor="postal-code"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                ZIP / Postal code
+                              </label>
+                              <input
+                                required
+                                type="text"
+                                name="postal-code"
+                                id="postal-code"
+                                autoComplete="postal-code"
+                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
+                          </>
+                        ) : null}
+
                         <div className="col-span-6 sm:col-start-1 col-end-7">
                           <label className="block text-sm font-medium leading-6 text-gray-900">
                             Pick Date
@@ -260,13 +338,24 @@ export default function Book() {
                         </div>
                       </div>
                       <div className="bg-gray-50 mt-5 px-4 py-3 text-right sm:px-6">
-                        <Paypal
-                          isDisabled={formValid}
-                          cost={price}
-                          date={selectedDate}
-                          desc={desc}
-                          userId={session.id}
-                        />
+                        {promo ? (
+                          <button
+                            type="submit"
+                            onClick={handleSubmit}
+                            className="group relative flex w-full justify-center rounded-md bg-purple-700 py-2 px-3 text-sm font-semibold text-white hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            aria-label="Subscribe"
+                          >
+                            Book
+                          </button>
+                        ) : (
+                          <Paypal
+                            isDisabled={formValid}
+                            cost={price}
+                            date={selectedDate}
+                            desc={desc}
+                            userId={session.id}
+                          />
+                        )}
                       </div>
                     </div>
                   </div>
@@ -293,18 +382,18 @@ export default function Book() {
   return (
     <>
       <Header isBooking={true} />
-      <div class="animate-pulse md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
+      <div className="animate-pulse md:shadow-xl lg:pt-32 pt-32 pb-12 lg:p-80 md:p-10 md:pt-40 md:pb-20 w-50">
         <div>
-          <div class="md:grid md:grid-cols-3 md:gap-6">
-            <div class="md:col-span-1">
-              <div class="px-4 sm:px-4">
+          <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="md:col-span-1">
+              <div className="px-4 sm:px-4">
                 <h2 className="h-40 bg-slate-700 rounded lg:text-2xl xl:text-6xl text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
                   <span className="h-20 bg-slate-700 rounded bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-blue-500"></span>
                 </h2>
-                <p class="h-10 bg-slate-700 rounded mt-1 text-sm text-gray-600"></p>
+                <p className="h-10 bg-slate-700 rounded mt-1 text-sm text-gray-600"></p>
               </div>
             </div>
-            <div class="mt-5 md:col-span-2 md:mt-0">
+            <div className="mt-5 md:col-span-2 md:mt-0">
               <form>
                 <div className="overflow-hidden shadow sm:rounded-md">
                   <div className="bg-white px-4 py-5 sm:p-1">
@@ -337,14 +426,13 @@ export default function Book() {
   );
 }
 
-
-async function checkAvailability(reqDate) {
+async function checkAvailability(reqDate, reqDesc) {
   const response = await fetch("/api/checkDate", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ reqDate }),
+    body: JSON.stringify({ reqDate, reqDesc }),
   });
 
   if (response.ok) {
@@ -355,5 +443,3 @@ async function checkAvailability(reqDate) {
     console.log("Error checking time slot");
   }
 }
-
-
