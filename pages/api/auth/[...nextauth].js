566b182ec573c84c95e56ffaff5d8ed7b8fba69e
glennanj1 <glennan20@gmail.com>
2023-05-20 22:39:31 -0400

    removed comments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,7 +21,7 @@ export const authOptions = {
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
-      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+      //maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
     }),
   ],
   callbacks: {
