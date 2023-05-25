3820be5dd841fd3ee5ce313c00831e16b67360bc
glennanj1 <glennan20@gmail.com>
2023-05-24 22:18:08 -0400

    header update

--- Components/Header.js
+++ Components/Header.js
@@ -127,7 +127,7 @@ function Header({isBooking}) {
               <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
                 <Link href='/booking' className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">
-                  All Bookings
+                  My Bookings
                 </Link>
               </li>
               {tabs.map(t => {
